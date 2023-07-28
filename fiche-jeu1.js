75e7fb240fa3802012e24c1539a61ec884285150
WOLFF Alexandre <alex.wolff97437@gmail.com>
2023-07-28 18:30:08 +0400

    add fiche jeu1.js and tous les jeux.js

---
+++ fiche-jeu1.js
@@ -0,0 +1,13 @@
+// add button subscribe to red and green
+const subscribe = document.getElementById('subscribe');
+
+const setColorGreen = () => {
+    subscribe.style.background = 'green'
+}
+
+const setColorRed = () => {
+    subscribe.style.background = 'red'
+}
+
+subscribe.addEventListener('mouseenter', setColorGreen);
+subscribe.addEventListener('mouseleave', setColorRed);
