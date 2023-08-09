0f00ddc756aaad2c98c5a869336a700bccc599f1
WOLFF Alexandre <alex.wolff97437@gmail.com>
2023-08-09 22:33:20 +0400

    update formulaire inscription

--- formulaire-inscription.js
+++ formulaire-inscription.js
@@ -3,13 +3,13 @@ const register = document.getElementById('register-form');
 register.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    let civility = event.target.querySelector('input[name="civility"]:checked').value; // pas d'espace pour checked sinon sa ne fonctionne pas
-    let name = event.target.querySelector('#name').value;
-    let firstname = event.target.querySelector('#firstname').value;
-    let newsletter = event.target.querySelector('#newsletter').checked;
+   let civility = event.target.querySelector('input[name="civility"]:checked').value; // pas d'espace pour checked sinon sa ne fonctionne pas
+   let name = event.target.querySelector('#name').value;
+   let firstname = event.target.querySelector('#firstname').value;
+   let newsletter = event.target.querySelector('#newsletter').checked;
 
-    let displayedCivility = civility === 'female' ? 'Mme' : 'M.'; // Si female = Mme sinon M.
-    let newsletterMessage;
+   let displayedCivility = civility === 'female' ? 'Mme' : 'M.'; // Si female = Mme sinon M.
+   let newsletterMessage;
 
     if (newsletter) {
         newsletterMessage = 'Merci de vous être abonné à notre newsletter'}
@@ -30,6 +30,24 @@ password.addEventListener('input', (event) => {
         errorPassword.innerText = ''
     }
 });
+
+ //Pour confirmation mot de passe
+const confirmPassword = document.getElementById('confirmPassword')
+const errorConfirmPassword = document.getElementById('errorConfirmPassword')
+
+function validatePassword(inputpassword, inputConfirmPassword) {
+    if (inputpassword.value !== inputConfirmPassword.value) {
+        return 'Les mots de passe ne correspondent pas';
+    }
+    return ''
+};
+
+
+confirmPassword.addEventListener('input', () => {
+    const errorMessage = validatePassword(password, confirmPassword);
+    errorConfirmPassword.innerText = errorMessage
+});
+
 
 // Expression régulière pour valider E-mail
 const regexEmail = /^([0-9a-zA-Z].*?@([0-9a-zA-Z].*\.\w{2,4}))$/
@@ -78,3 +96,5 @@ identifiant.addEventListener('input', (event) => {
     const errorMessage = validIdentifiant(event.target.value);
     errorIdentifiant.innerText = errorMessage
 });
+
+
