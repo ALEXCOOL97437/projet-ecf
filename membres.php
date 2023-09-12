08037bdafbda31f410e44181662b1989c14d9965
WOLFF Alexandre <alex.wolff97437@gmail.com>
2023-09-12 16:32:31 +0400

    update espace admin + membres and add gerer membres + publier membres

--- membres.php
+++ membres.php
@@ -23,7 +23,13 @@ if(!$_SESSION['mdp']){
    $recupUsers = $bdd->query('SELECT * FROM users');
    while($user = $recupUsers->fetch()){ // Boucle pour récupérer les membres de la table
     ?>
-    <p><?php echo $user['pseudo']; ?> <a href="bannir.php?id=<?= $user['id']; ?>" style="color:red;">Bannir le membre</a></p> <!-- ce code affiche le pseudo de l'utilisateur dans un paragraphe HTML et fournit également un lien hypertexte vers un script de bannissement en utilisant l'ID de l'utilisateur comme paramètre dans l'URL. -->
+ <div class="membres" style="border: 1px solid black;">
+    <p><?php echo $user['pseudo']; ?></p>
+     <a href="bannir.php?id=<?= $user['id']; ?>">
+     <button style="color:white; background-color: red; margin-bottom: 10px;">Bannir le membre</button></a> <!-- ce code affiche le pseudo de l'utilisateur dans un paragraphe HTML et fournit également un lien hypertexte vers un script de bannissement en utilisant l'ID de l'utilisateur comme paramètre dans l'URL. -->
+    <a href="gerer-membres.php?id=<?= $user['id']; ?>">
+    <button style="color:white; background-color: blue; margin-bottom: 10px;">Modifier le membre</button></a></p>
+ </div>
     <?php
    }
 ?>
