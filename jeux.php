299ac198eafd41b7a37e9a13caf4b491f9acc321
WOLFF Alexandre <alex.wolff97437@gmail.com>
2023-09-13 16:10:28 +0400

    add supprimer jeux AND update jeux.php

--- jeux.php
+++ jeux.php
@@ -25,7 +25,7 @@ if(!$_SESSION['mdp']){
     ?>
  <div class="jeux" style="border: 1px solid black;">
     <p><?php echo $games['titre']; ?></p>
-     <a href="supprimer-jeu.php?id=<?= $games['id']; ?>">
+     <a href="supprimer-jeux.php?id=<?= $games['id']; ?>">
      <button style="color:white; background-color: red; margin-bottom: 10px;">Supprimer le jeu</button></a> <!-- ce code affiche le pseudo de l'utilisateur dans un paragraphe HTML et fournit également un lien hypertexte vers un script de bannissement en utilisant l'ID de l'utilisateur comme paramètre dans l'URL. -->
     <a href="gerer-jeux.php?id=<?= $games['id']; ?>">
     <button style="color:white; background-color: blue; margin-bottom: 10px;">Modifier le jeu</button></a></p>
