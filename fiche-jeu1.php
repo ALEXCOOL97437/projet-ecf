3e36d032f5d0a71baef643dc035ffe3efd77b896
WOLFF Alexandre <alex.wolff97437@gmail.com>
2023-09-14 21:25:22 +0400

    add fiche jeux.css a,d tous les jeux.php AND update fivhe jeu1.php

--- fiche-jeu1.php
+++ fiche-jeu1.php
@@ -36,9 +36,9 @@ if ($requete->rowCount() > 0) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $jeu['titre']; ?></title>
-    <link rel="stylesheet" href="fiche-jeu1.css">
+    <link rel="stylesheet" href="fiche-jeux.css">
 </head>
-<body>
+<body style="background-image: url('IMAGES/IMAGE\ ARRIERE\ PLAN.png'); background-size: cover;">
     <header>
         <nav class="menu">
             <ul>
@@ -50,59 +50,50 @@ if ($requete->rowCount() > 0) {
             <div class="connect"><a href="connexion-gamesoft.php"><img src="IMAGES/LOGO SE CONNECTER.png" alt=""></a></div>
         </nav>
     </header>
-        <div class="bloc1"><img src="IMAGES/President devil main.png" alt=""></div>
-        <div class="bloc2">
-            <ul class="description">
-                <li>TITRE : <?php echo $games['titre'];?></li>
-                <br>
-                <li>STUDIO : <?php echo $games['studio'];?></li>
-                <br>
-                <li>SUPPORT : <?php echo $games['support'];?></li>
-                <br>
-                <li>PRIORITE DEVELOPPEMENT : <?php echo $games['priorite_developpement'];?></li>           
-                <br>
-                <li>MOTEUR : <?php echo $games['moteur'];?></li>
-                <br>
-                <li>DATE DE CREATION : <?php echo $games['date_creation'];?></li>
-                <br>
-                <li>DERNIERE MAJ : <?php echo $games['date_maj'];?></li>
-                <br>
-                <li>DATE DE SORTIE : <?php echo $games['date_sortie'];?></li>
-                <br>
-                <li>BUDGET : <?php echo $games['budget'];?></li>
-                <br>
-                <li>STATUT : <?php echo $games['statut'];?></li>
-                <br>
-                <li>TYPE : <?php echo $games['type'];?></li>
-                <br>
-                <li>NOMBRE DE JOUEUR : <?php echo $games['nombre_joueurs'];?></li>
-                <br>
-                <li>Score : <?php echo $likes; ?></li>
-                <br>
-                <li><a href="favoris.php?t=1&id=<?php echo $games['id']; ?>">
-                <button style="color:white; background-color: green; margin-bottom: 10px;">Mettre en favoris</button>
-                </a></li>
-                <li><a href="favoris.php?t=2&id=<?php echo $games['id']; ?>">
-                <button style="color:black; background-color: yellow; margin-bottom: 10px;">Supprimer favoris</button>
-                </a></li>
-            </ul>
-        </div>
-        <br>
-        <br>
-  <h2>A propos du jeu</h2>
-  <p class="descriptif">Six ans se sont écoulés depuis la catastrophe biologique de Raccoon City. L'agent Leon S. Kennedy, 
-    l'un des survivants du drame, est envoyé secourir la fille du président qui a été kidnappée. 
-    Son enquête le mène jusqu'à un village européen isolé, dont les habitants cachent un effroyable 
-    secret. Et ainsi se lève le rideau d'une histoire de sauvetage audacieux et d'horreur atroce, où
-    se mêlent vie, mort, terreur et catharsis.
-    Avec un gameplay modernisé, une histoire revisitée et des graphismes ultra détaillés, 
-    President Devil 4 signe la renaissance d'un monstre de l'industrie.
-    Replongez dans le cauchemar qui a révolutionné les jeux d'horreur et de survie. </p>
-    <div class="blocs">
-        <div class="image1"><a href="IMAGES/President devil1.png"><img src="IMAGES/President devil1.png" alt=""></a></div>
-        <div class="image2"><a href="IMAGES/President devil2.png"><img src="IMAGES/President devil2.png" alt=""></a></div>
-        <div class="image3"><a href="IMAGES/President devil3.png"><img src="IMAGES/President devil3.png" alt=""></a></div>
+    <div class="bloc-container">
+            <div class="bloc1"><img src="IMAGES/President devil main.png" alt=""></div>
+            <div class="bloc2">
+                <ul class="description" style="color:ghostwhite">
+                    <li>TITRE : <?php echo $games['titre'];?></li>
+                    <br>
+                    <li>STUDIO : <?php echo $games['studio'];?></li>
+                    <br>
+                    <li>SUPPORT : <?php echo $games['support'];?></li>
+                    <br>
+                    <li>PRIORITE DEVELOPPEMENT : <?php echo $games['priorite_developpement'];?></li>           
+                    <br>
+                    <li>MOTEUR : <?php echo $games['moteur'];?></li>
+                    <br>
+                    <li>DATE DE CREATION : <?php echo $games['date_creation'];?></li>
+                    <br>
+                    <li>DERNIERE MAJ : <?php echo $games['date_maj'];?></li>
+                    <br>
+                    <li>DATE DE SORTIE : <?php echo $games['date_sortie'];?></li>
+                    <br>
+                    <li>STATUT : <?php echo $games['statut'];?></li>
+                    <br>
+                    <li>TYPE : <?php echo $games['genre'];?></li>
+                    <br>
+                    <li>NOMBRE DE JOUEUR : <?php echo $games['nombre_joueurs'];?></li>
+                    <br>
+                    <li>Score : <?php echo $likes; ?></li>
+                    <br>
+                    <li><a href="favoris.php?t=1&id=<?php echo $games['id']; ?>">
+                    <button style="color:white; background-color: green; margin-bottom: 10px;">Mettre en favoris</button>
+                    </a></li>
+                    <li><a href="favoris.php?t=2&id=<?php echo $games['id']; ?>">
+                    <button style="color:black; background-color: yellow; margin-bottom: 10px;">Supprimer favoris</button>
+                    </a></li>
+                </ul>
+            </div>
     </div>
+        <h2>A propos du jeu</h2>
+            <p class="descriptif" style="color: ghostwhite;"><?php echo $games['descriptif']; ?> </p>
+            <div class="blocs">
+                <div class="image1"><a href="IMAGES/President devil1.png"><img src="IMAGES/President devil1.png" alt=""></a></div>
+                <div class="image2"><a href="IMAGES/President devil2.png"><img src="IMAGES/President devil2.png" alt=""></a></div>
+                <div class="image3"><a href="IMAGES/President devil3.png"><img src="IMAGES/President devil3.png" alt=""></a></div>
+            </div>
     <script src="fiche-jeu1.js"></script>
 </body>
 </html>
---
+++ tous-les-jeux.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GAMESOFT-TOUS-LES-JEUX</title>
+    <link rel="stylesheet" href="tous-les-jeux.css">
+</head>
+<body>
+    <header>
+        <nav class="menu">
+            <ul>
+            <li class="logo"><img src="IMAGES/LOGO.png" alt=""></li>
+            <li><a href="gamesoft-page-accueil.php">Accueil</a></li>
+            <li><a href="">JEUX</a></li>
+            <li><button id="subscribe" type="button"><a href="inscription-gamesoft.php">S'INSCRIRE</a></button></li>
+            </ul>
+            <div class="connect"><a href="connexion-gamesoft.php"><img src="IMAGES/LOGO SE CONNECTER.png" alt=""></a></div>
+        </nav>
+    </header>
+    <div class="blocs">
+        <div class="jeu1"><a href="fiche-jeu1.php"><img src="IMAGES/IMAGE ARRIERE PLAN.png" alt=""></a></div>
+        <div class="jeu2"><a href=""><img src="IMAGES/IMAGE ARRIERE PLAN.png" alt=""></a></div>
+        <div class="jeu3"><a href=""><img src="IMAGES/IMAGE ARRIERE PLAN.png" alt=""></a></div>
+        <div class="jeu4"><a href=""><img src="IMAGES/IMAGE ARRIERE PLAN.png" alt=""></a></div>
+        <div class="jeu5"><a href=""><img src="IMAGES/IMAGE ARRIERE PLAN.png" alt=""></a></div>
+        <div class="jeu6"><a href=""><img src="IMAGES/IMAGE ARRIERE PLAN.png" alt=""></a></div>
+        <div class="jeu7"><a href=""><img src="IMAGES/IMAGE ARRIERE PLAN.png" alt=""></a></div>
+        <div class="jeu8"><a href=""><img src="IMAGES/IMAGE ARRIERE PLAN.png" alt=""></a></div>
+        <div class="jeu9"><a href=""><img src="IMAGES/IMAGE ARRIERE PLAN.png" alt=""></a></div>
+        <div class="jeu10"><a href=""><img src="IMAGES/IMAGE ARRIERE PLAN.png" alt=""></a></div>
+    </div>
+    <script src="tous-les-jeux.js"></script>
+</body>
+</html>
